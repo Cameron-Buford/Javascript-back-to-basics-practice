1d3cf348bc7a29a868d28f33b41e02cd1ec1c049
unknown <cambuf@gmail.com>
2020-05-04 14:39:29 -0600

    kata-listfiltering

--- jsBasics.js
+++ jsBasics.js
@@ -659,4 +659,40 @@ const popItem = (arra) => {
 
 
 
+//CODEWARS 
+
+//KATA 7--- LIST FILTERING
+
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+// Example
+// filter_list([1,2,'a','b']) == [1,2]
+// filter_list([1,'a','b',0,15]) == [1,0,15]
+let l = [1,2,'aasf','1','123',123]; 
+//== [1,2,123]
+
+function filter_list(l) {
+  let newArray = [];
+  for(let i = 0; i < l.length; i ++){
+    if(typeof l[i] === 'number'){
+      newArray.push(l[i]);
+    }
+  }
+  return newArray;
+  
+}
+
+// var filterArray = [1,2,'aasf','1','123',123];
+
+// filter_list = (filterArray) => {
+//   return filterArray.filter(x => typeof x === 'number')
+// }
+// console.log(filter_list())
+
+
+
+
+
+
+
 
